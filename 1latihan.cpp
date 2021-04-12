59c0605c73dff0813fe9df4a40dc9795950957ed
haliputri <77369540+haliputri@users.noreply.github.com>
2021-04-12 14:54:34 +0700

    Add files via upload

---
+++ 1latihan.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+struct Mahasiswa {
+    char NPM[14];
+    char nama[30];
+    int nilai;
+};
+
+typedef Mahasiswa* pointer;
+
+int main(){
+    pointer p, q;
+    p = new Mahasiswa;
+    cout << "Nama   : " ; cin >> p->nama;
+    cout << "NPM    : " ; cin >> p->NPM;
+    cout << "Nilai  : " ; cin >> p->nilai;
+    cout << endl;
+    q = new Mahasiswa;
+    cout << "Nama   : " ; cin >> q->nama;
+    cout << "NPM    : " ; cin >> q->NPM;
+    cout << "Nilai  : " ; cin >> q->nilai;
+    cout << endl;
+
+    cout << "Mahasiswa" << endl;
+    cout << p->NPM << " " << p->nama << " " << p->nilai << endl;
+    cout << q->NPM << " " << q->nama << " " << q->nilai << endl;
+
+    delete(p);
+    delete(q);
+}
